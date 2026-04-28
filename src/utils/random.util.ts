108ccf9669b455110121dbda416361acccb916c4
fullstackdeveloper710 <fullstackdeveloper710@gmail.com>
2026-04-28 22:39:33 +0530

    removed unused code

--- src/utils/random.util.ts
+++ src/utils/random.util.ts
@@ -1,15 +1,5 @@
 import crypto from 'crypto';
 
-/**
- * Generate a random 6-digit OTP
- */
-export const generateOTP = (): string => {
-    return Math.floor(100000 + Math.random() * 900000).toString();
-};
-
-/**
- * Generate a secure random token
- */
 export const generateToken = (): string => {
     return crypto.randomBytes(32).toString('hex');
 };
